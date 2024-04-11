63a127f325996af7bb0f8d22bdfe255a1090d1ce
vivekaschittimutti <155310310+vivekaschittimutti@users.noreply.github.com>
2024-04-11 23:40:45 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,115 +1,66 @@
-let inputEl = document.getElementById("searchInput");
 let resultCountries = document.getElementById("resultCountries");
-let spinnerEl = document.getElementById("spinner");
 
-function allcountries(eachCountry) {
-    let resultContainerEl = document.createElement("div");
-    resultContainerEl.classList.add('countryContainer');
-    resultContainerEl.classList.add("d-flex", "flex-row", "col-md-5", "col-lg-5", 'mb-3', "ml-2");
-    resultCountries.appendChild(resultContainerEl);
-    let {
-        name,
-        population,
-        flag
-    } = eachCountry;
-    let searchCountryValue = inputEl.value;
+let searchInputEl = document.getElementById("searchInput");
 
+let spinnerEl = document.getElementById("spinner")
+
+let countryList = [];
+
+function createAndAppendCountry(country) {
+    spinnerEl.classList.add('d-none');
+    let containerEl = document.createElement("div");
+    containerEl.classList.add("d-flex", 'flex-row', 'col-11', 'col-md-5', 'ml-auto', 'mr-auto', 'countryContainer');
+    resultCountries.appendChild(containerEl);
     let flagEl = document.createElement("img");
-    flagEl.src = flag;
-    flagEl.classList.add("countryflag");
-    resultContainerEl.appendChild(flagEl);
-    let rowContainer = document.createElement("div");
-    rowContainer.classList.add('rowContainer');
-    resultContainerEl.appendChild(rowContainer);
-    let countrynameEl = document.createElement("h1");
-    countrynameEl.textContent = name;
-    countrynameEl.classList.add("countryName");
-    rowContainer.appendChild(countrynameEl);
-    let countryPopulationEl = document.createElement("p");
-    countryPopulationEl.textContent = population;
-    rowContainer.appendChild(countryPopulationEl);
-}
+    flagEl.classList.add('image');
+    flagEl.src = country.flag;
+    containerEl.appendChild(flagEl);
+    let subContainer = document.createElement("div");
+    subContainer.classList.add("subContainer");
+    containerEl.appendChild(subContainer);
+    let countryName = document.createElement('h1');
+    countryName.classList.add("countryName");
+    countryName.textContent = country.name;
+    subContainer.appendChild(countryName);
+    let population = document.createElement("p");
+    population.textContent = country.population;
+    subContainer.appendChild(population);
 
-function addCountry(country) {
-    spinnerEl.classList.toggle("d-none");
-    for (let item of country) {
-        allcountries(item);
-    }
 
 }
 
-function allcountries2(eachCountry) {
+function displaySearchResults(jsondata) {
 
-    let {
-        name,
-        population,
-        flag
-    } = eachCountry;
-    let searchCountryValue = inputEl.value;
-    if (name === searchCountryValue) {
-        let resultContainerEl = document.createElement("div");
-        resultContainerEl.classList.add('countryContainer');
-        resultContainerEl.classList.add("d-flex", "flex-row", "col-md-6", 'col-lg-6');
-        resultCountries.appendChild(resultContainerEl);
-        let subcontainer = document.createElement("div");
-        subcontainer.classList.add("m-2");
-        resultContainerEl.appendChild('subcontainer');
-        let flagEl = document.createElement("img");
-        flagEl.src = flag;
-        flagEl.classList.add("countryflag");
-        subcontainer.appendChild(flagEl);
-        let rowContainer = document.createElement("div");
-        rowContainer.classList.add('rowContainer');
-        subcontainer.appendChild(rowContainer);
-        let countrynameEl = document.createElement("h1");
-        countrynameEl.textContent = name;
-        countrynameEl.classList.add("countryName");
-        rowContainer.appendChild(countrynameEl);
-        let countryPopulationEl = document.createElement("p");
-        countryPopulationEl.textContent = population;
-        rowContainer.appendChild(countryPopulationEl);
+    for (let country of countryList) {
+        createAndAppendCountry(country);
     }
 }
 
-function addCountry1(data) {
-    spinnerEl.classList.toggle("d-none");
-    for (let item of data) {
-        allcountries2(item);
+function getresults() {
+    spinnerEl.classList.remove('d-none');
+    let options = {
+        method: "GET"
     }
-
+    let url = "https://apis.ccbp.in/countries-data";
+    fetch(url, options)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(jsondata) {
+            countryList = jsondata;
+            displaySearchResults(jsondata);
+        })
 }
+let searchCountry = function(event) {
+    resultCountries.textContent = '';
+    let searchCountry = event.target.value;
 
-
-function searchCountry(event) {
-    spinnerEl.classList.toggle("d-none");
-    if (event.key === "Enter") {
-        resultCountries.textContent = '';
-        let searchCountryValue = inputEl.value;
-        let url = "https://apis.ccbp.in/countries-data?= " + searchCountryValue;
-        let options = {
-            method: "GET"
-        }
-        fetch(url, options)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(data) {
-                addCountry1(data);
-            });
+    for (let country of countryList) {
+        let countryName = country.name;
+        if (countryName.includes(searchCountry))
+            createAndAppendCountry(country);
     }
 }
 
-let options = {
-    method: "GET"
-}
-spinnerEl.classList.toggle("d-none");
-let url = "https://apis.ccbp.in/countries-data";
-fetch(url, options)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        let country = data;
-        addCountry(country);
-    });
-inputEl.addEventListener('keydown', searchCountry);
+getresults();
+searchInputEl.addEventListener('keyup', searchCountry);
